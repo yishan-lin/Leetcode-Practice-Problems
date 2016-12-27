86e0a032ab3c25cda7777531ad99fab79aaa2184
ylin10 <ylin10@bu.edu>
2016-12-26 23:12:01 -0800

    Completed brute force solution for TwoSum
    I overthought it, thinking that hash map was a necessity and the
    original solution given was only applicable if x and y were next to
    each other.
    
    Just need to take a step back and do things slower.  Solution is much
    simpler, just iterate through array once, then based on the input
    target int, find the difference, then go through the rest of the array
    looking for the difference in value.

--- TwoSum.java
+++ TwoSum.java
@@ -1,10 +1,33 @@
 public class TwoSum {
     
-    public static void main(String[] args) {
+    public static int[] twosum(int[] array, int target) {
         
-        int[] nums = {2, 7, 11, 15};
+        for (int x = 0; x < array.length; x++) {
+            for (int y = x + 1; y < array.length; y++) {
+                
+                int diff = target - array[x];
+                
+                if (array[y] == diff) {
+                    int[] solution = {x, y};
+                    System.err.println("x = " + x);
+                    System.err.println("y = " + y);
+                    return solution;
+                }
+                
+            }
+            
+        }
         
+        return null;
         
     }
-
+    
+    public static void main (String[] args) {
+        
+        int[] array = {2, 7, 11, 15};
+        
+        System.out.println("solution = " + twosum(array, 9));
+        
+    }
+    
 }
