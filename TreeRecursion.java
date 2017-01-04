e07156225d6ff1d401bfd8a4ead70fd8707c3368
ylin10 <ylin10@bu.edu>
2017-01-04 07:54:31 -0800

    Rewriting/Testing Tree
    Test of ability to build from scratch.

--- TreeRecursion.java
+++ TreeRecursion.java
@@ -3,91 +3,169 @@ public class TreeRecursion {
     public static class Node {
         
         int item;
-        Node right;
         Node left;
+        Node right;
         
         public Node(int x) {
             
             item = x;
-            right = null;
             left = null;
+            right = null;
             
         }
         
-        public Node(int x, Node r, Node l) {
+        public Node(int x, Node l, Node r) {
             
             item = x;
-            right = r;
             left = l;
+            right = r;
             
         }
-        
     }
     
     public static Node insert(int x, Node root) {
-        
-        if (root == null) { //if the root is empty, add new Node instantly
+     
+        if (root == null) {
             return new Node(x);
         }
+        
         else if (root.item > x) {
-            root.left = insert(x, root.left);   
+            return insert(x, root.left);
         }
+        
         else if (root.item < x) {
-            root.right = insert(x, root.right);
-        }
-        return root;
-    }
-    
-    public static Node convertarraytotree(int[] array) {
-     
-        Node root = null;
-        
-        for (int x = 0; x < array.length; x++) {
-         
-            root = insert(array[x], root);
-            
-        }
-        
-        return root;
-        
-    }
-    
-    public static void preordertraversal(Node root) {
-     
-        if (root == null) {
-            System.out.print("");
-        }  
-        else {
-            System.out.print(root.item + ", ");
-            preordertraversal(root.left);
-            preordertraversal(root.right);
-        }
-        
-    }
-    
-    public static int printleftleavessum(Node root) {
-     
-        if (root == null) {
-            return 0;
-        }
-        
-        else if (root.left.left == null && root.left.right == null && root.left !=null) {
-            return (1 + printleftleavessum(root.right) + printleftleavessum(root.left));
+            return insert(x, root.right);
         }
         
         else {
-            return printleftleavessum(root.right) + printleftleavessum(root.left);
+            return root;
         }
         
     }
     
     public static void main(String[] args) {
         
-        Node root1 = null;
-        int[] A = {3, 9, 20, 15, 7};
-        root1 = convertarraytotree(A);
-        
-        preordertraversal(root1);
         
     }
 }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /* public static class Node {
+         * 
+         int item;
+         Node right;
+         Node left;
+         
+         public Node(int x) {
+         
+         item = x;
+         right = null;
+         left = null;
+         
+         }
+         
+         public Node(int x, Node r, Node l) {
+         
+         item = x;
+         right = r;
+         left = l;
+         
+         }
+         
+         }
+         
+         public static Node insert(int x, Node root) {
+         
+         if (root == null) { //if the root is empty, add new Node instantly
+         return new Node(x);
+         }
+         else if (root.item > x) {
+         root.left = insert(x, root.left);   
+         }
+         else if (root.item < x) {
+         root.right = insert(x, root.right);
+         }
+         return root;
+         }
+         
+         public static Node convertarraytotree(int[] array) {
+         
+         Node root = null;
+         
+         for (int x = 0; x < array.length; x++) {
+         
+         root = insert(array[x], root);
+         
+         }
+         
+         return root;
+         
+         }
+         
+         public static void preordertraversal(Node root) {
+         
+         if (root == null) {
+         System.out.print("");
+         }  
+         else {
+         System.out.print(root.item + ", ");
+         preordertraversal(root.left);
+         preordertraversal(root.right);
+         }
+         
+         }
+         
+         public static int printleftleavessum(Node root) {
+         
+         if (root == null) {
+         return 0;
+         }
+         
+         else if (root.left.left == null && root.left.right == null && root.left !=null) {
+         return (1 + printleftleavessum(root.right) + printleftleavessum(root.left));
+         }
+         
+         else {
+         return printleftleavessum(root.right) + printleftleavessum(root.left);
+         }
+         
+         }
+         
+         public static void main(String[] args) {
+         
+         Node root1 = null;
+         int[] A = {3, 9, 20, 15, 7};
+         root1 = convertarraytotree(A);
+         
+         preordertraversal(root1);
+         
+         }
+         }*/
