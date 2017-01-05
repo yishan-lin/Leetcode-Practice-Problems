ec1d474703ba6eb66c247cb2e51e1a20cc31b2aa
ylin10 <ylin10@bu.edu>
2017-01-05 09:32:37 -0800

    Continued practice of Hash Tables

--- HashTablePractice.java
+++ HashTablePractice.java
@@ -1,18 +1,34 @@
 import java.util.Hashtable;
+import java.util.Enumeration;
 
 public class HashTablePractice {
- 
+    
     public static void main(String[] args) {
-     
-    Hashtable practice = new Hashtable();
-    
-    practice.put("Apple", "Cupertino");
-    
-    System.out.println("Does the hashtable contain Apple (should be true) = " + practice.containsKey("Apple"));
-    System.out.println("Does the hashtable contain Google (should be false) = " + practice.containsKey("Google"));
-    System.out.println("Does the hashtable contain the VALUE of Coop = " + practice.containsValue("Cupertino"));
-    
+        
+        Hashtable<String, String> companies = new Hashtable<String, String>();
+        
+        Enumeration iterator;
+        Enumeration valueiterator;
+
+        companies.put("Apple", "Cupertino");
+        companies.put("Uber", "San Francisco");
+        companies.put("Oracle", "Redwood City");
+                        
+        iterator = companies.keys();
+        valueiterator = companies.elements();
+
+        System.out.println("should be true = " + companies.containsKey("Apple"));
+        System.out.println("should be false = " + companies.containsValue("Saratoga"));
+        
+       System.out.println("should be true if element = " + iterator.hasMoreElements());
+       
+       while (iterator.hasMoreElements()) {
+        
+           System.out.println(iterator.nextElement() + " : " + valueiterator.nextElement());
+           
+       }
+        
+        
     }
-    
     
 }
