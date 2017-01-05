40a8326f558ec8beea8e393f67491f38c6642383
ylin10 <ylin10@bu.edu>
2017-01-05 10:30:15 -0800

    Attempting 2 methods that don't work...
    - Need to understand Enumeration, Character, String and how they work
    in HashTable

--- HashTableDuplicateString.java
+++ HashTableDuplicateString.java
@@ -1,33 +1,67 @@
 import java.util.Hashtable;
+import java.util.Enumeration;
 
 public class HashTableDuplicateString {
     
     public static void duplicate(String x) {
         
-       Hashtable<Character, Integer>duplicate = new Hashtable<Character, Integer>();
-       
-       char[] chararray = new char[x.length()];
-       
-       chararray = x.toCharArray();
-       
+        Hashtable<Character, Integer>duplicate = new Hashtable<Character, Integer>();
+        
+        char[] chararray = new char[x.length()];
+        
+        chararray = x.toCharArray();
+        
         for (int count = 0; count < chararray.length; count++) {
             
-            System.out.println(chararray[count]);
+            //for debug purposes
+            //System.out.println(chararray[count]);
+            
+            if (duplicate.containsKey(chararray[count])) {
+                
+                duplicate.put(chararray[count], duplicate.get(chararray[count]) + 1);
+                
+            }
+            
+            else {
+                
+                duplicate.put(chararray[count], 1);
+                
+            }
+            
+        }
         
-           if (duplicate.containsKey(chararray[count])) {
-               
-               duplicate.put(chararray[count], duplicate.get(chararray[count]) + 1);
-               
-           }
-           
-           
-       }
-       
+        Enumeration hashtablekeys = duplicate.keys();
+        Enumeration hashtablevalues = duplicate.elements();
+        
+        while (hashtablekeys.hasMoreElements()) { //main traversal of hash table keys
+            
+            //Character keycomparison = hashtablekeys.nextElement();
+            
+            if (duplicate.get(hashtablekeys.nextElement()) > 1) {
+                
+                System.out.println("DUPLICATE SPOTTED! = " + duplicate.keys());
+                
+            }
+            
+            else {
+                
+                continue;
+                
+            }
+            
+        }
+        
+        /* if (duplicate.get(chararray[count]) > 1) {
+         * 
+         System.out.println("Here are the duplicate characters in a String = " + chararray[count]);
+         
+         }*/
+        
     }
     
     public static void main(String[] args) {
         
-        duplicate("yo");
+        duplicate("yoy");
         
     }
     
