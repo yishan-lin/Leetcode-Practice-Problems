a3161883e01348bb469a3fc95bee933dc640744a
ylin10 <ylin10@bu.edu>
2017-01-06 07:21:41 -0800

    Test of Duplicate String

--- HashTableDuplicateString.java
+++ HashTableDuplicateString.java
@@ -1,88 +1,58 @@
 import java.util.Hashtable;
-import java.util.Enumeration;
 import java.util.Set;
+import java.util.Enumeration;
 
 public class HashTableDuplicateString {
     
-    public static void duplicate(String x) {
-        
+    //find duplicate in a string
+    
+    static void duplicate_char(String string) {
+     
         Hashtable<Character, Integer>duplicate = new Hashtable<Character, Integer>();
         
-        char[] chararray = new char[x.length()];
+        char[] char_array = new char[string.length()];
+            
+        char_array = string.toCharArray();
         
-        chararray = x.toCharArray();
-        
-        for (int count = 0; count < chararray.length; count++) {
-            
-            //for debug purposes
-            //System.out.println(chararray[count]);
-            
-            if (duplicate.containsKey(chararray[count])) {
-                
-                duplicate.put(chararray[count], duplicate.get(chararray[count]) + 1);
-                
-            }
-            
-            else {
-                
-                duplicate.put(chararray[count], 1);
-                
-            }
-            
-        }
-        
-        //Enumeration hashtablekeys = duplicate.keys();
-        //Enumeration hashtablevalues = duplicate.elements();
-        
-        Set<Character> characterset = duplicate.keySet();
-        
-        System.out.println("Found duplicates in " + x);
-        
-        for (Character c : characterset) {
-            
-            if (duplicate.get(c) > 1) {
-                
-                System.out.println((c) + " : " + duplicate.get(c));
-                
-            }
-            
-            else {
-                
-                continue;
-                
-            }
-            
-        }
-        
-       /* while (hashtablekeys.hasMoreElements()) { //main traversal of hash table keys
-            
-            //Character keycomparison = hashtablekeys.nextElement();
-            
-            if (duplicate.get(hashtablekeys.nextElement()) > 1) {
-                
-                System.out.println("DUPLICATE SPOTTED! = ");
-                
-            }
-            
-            else {
-                
-                continue;
-                
-            }
-            
-        }*/
-        
-        /* if (duplicate.get(chararray[count]) > 1) {
-         * 
-         System.out.println("Here are the duplicate characters in a String = " + chararray[count]);
+        for (int x = 0; x < char_array.length; x++) {
          
-         }*/
+            //System.out.println("chararray[x] = " + char_array[x]);
+            
+            if (duplicate.containsKey(char_array[x])) {
+                
+                duplicate.put(char_array[x], 1 + duplicate.get(char_array[x]));
+            
+            }
+            
+            else {
+             
+                duplicate.put(char_array[x], 1);
+                
+            }
+            
+        }
+        
+        Set<Character> duplicate_char_keys = duplicate.keySet();
+        
+        System.out.println("All duplicates in String " + string);
+        
+        for (char y : duplicate_char_keys) {
+            
+            if (duplicate.get(y) > 1) {
+                
+                System.out.println("Char: " + y + " Count: " + duplicate.get(y));
+                
+            }
+            
+        }
+        
+        
         
     }
     
     public static void main(String[] args) {
         
-        duplicate("Javahelloiamhavingfun");
+        duplicate_char("Yoyoyoyoyo");
         
     }
     
