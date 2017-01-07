0cb53da0fa65fa0c385bbec80d86ac4bbf751764
ylin10 <ylin10@bu.edu>
2017-01-07 12:07:37 -0800

    Finished Hashtable Solution

--- isUnique.java
+++ isUnique.java
@@ -1,23 +1,40 @@
 /*Implement an algo to see if String has all unique characters*/
 
+import java.util.Hashtable;
+
 public class isUnique {
     
-    static void isUnique(String string) {
+    static boolean isUnique(String string) {
      
-        for (int x = 0; x < string.length(); x++) {
+        char[] chararray = string.toCharArray();
+        
+        Hashtable<Character, Boolean> duplicate = new Hashtable<Character, Boolean>();
+        
+        for (int x = 0; x < chararray.length; x++) {
          
-            System.out.println(string.charAt(x));
+            //System.out.println("PRINT = " + chararray[x]);
             
+            if (duplicate.containsKey(chararray[x])) {
+                
+                System.out.println("Duplicate spotted of " + chararray[x] + "!");
+                return false;
+                
+            }
             
+            else {
+               
+                duplicate.put(chararray[x], true);
+                
+            }
         }
         
-        
+        return true;
     }
     
     public static void main(String[] args) {
         
-        String string = "blahblahblah";
-        
+        String string = "isthisaunique";
+        System.out.println(isUnique(string));
     }
     
 }
