dba911e21c69423cfe75ea00e7bd488aaae0f3a7
ylin10 <ylin10@bu.edu>
2017-01-07 11:52:28 -0800

    isUnique
    /*Implement an algo to see if String has all unique characters*/

---
+++ isUnique.java
@@ -0,0 +1,23 @@
+/*Implement an algo to see if String has all unique characters*/
+
+public class isUnique {
+    
+    static void isUnique(String string) {
+     
+        for (int x = 0; x < string.length(); x++) {
+         
+            System.out.println(string.charAt(x));
+            
+            
+        }
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        String string = "blahblahblah";
+        
+    }
+    
+}
