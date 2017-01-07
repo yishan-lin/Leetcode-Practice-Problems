d1cce7d8d4766a6da47fc4872981fc28a504de68
ylin10 <ylin10@bu.edu>
2017-01-07 14:39:39 -0800

    URLify Final Solution

--- URLifywithCharAt.java
+++ URLifywithCharAt.java
@@ -1,8 +1,56 @@
 public class URLifywithCharAt {
     
     static void uRLify(String string) {
+        
+        char[] chararray = string.toCharArray();
+        
+        System.out.println(chararray.length);
+        
+        int whitespacecount = 0;
+        
+        for (char x : chararray) {
+         
+            if (x == ' ') {
+             
+                whitespacecount+= 1;
+                
+            }
+            
+        }
+        
+        System.out.println("whitespace count = " + whitespacecount);
+        
+        int solutionarraylength = chararray.length + whitespacecount * 3;
+        
+        char[] solutionarray = new char[solutionarraylength];
+        int y = solutionarraylength - 1;
+        
+        for (int x = chararray.length - 1; x >= 0; x--) {
+         
+            if (chararray[x] == ' ') {
+             
+                solutionarray[y] = '0';
+                solutionarray[y-1] = '2';
+                solutionarray[y-2] = '%';
+                y -= 3;
+                
+            }
+            
+            else {
+             
+                solutionarray[y] = chararray[x];
+                y-= 1;
+            }
+            
+        }
+        
+        for (int x = 0; x < solutionarray.length; x++) {
+         
+            System.out.print(solutionarray[x]);
+            
+        }
      
-        //first count the # of whitespaces, becaise we have to add 3 character spaces, the %20 to each whitespace
+      /*  //first count the # of whitespaces, becaise we have to add 3 character spaces, the %20 to each whitespace
         
         char[] chararray = string.toCharArray();
         
@@ -27,11 +75,11 @@ public class URLifywithCharAt {
         
         for (int x = chararray.length - 1; x >= 0; x--) {
          
-            System.out.println("chararray[x] = " + chararray[x]);
+            //System.out.println("chararray[x] = " + chararray[x]);
             
             if (chararray[x] == ' ') {
                 
-                System.out.println("whitespace spotted in program");
+                //System.out.println("whitespace spotted in program");
                 solutionarray[y] = '0';
                 solutionarray[y-1] = '2'; 
                 solutionarray[y-2] = '%';
@@ -55,12 +103,16 @@ public class URLifywithCharAt {
             
         }
         
+    }*/
+        
     }
     
     public static void main(String[] args) {
         
         String string = "Ms Jones Smith";
+        String stringtwo = "Introducing Mr Sam Altman";
         uRLify(string);
+        uRLify(stringtwo);
         
     }
     
