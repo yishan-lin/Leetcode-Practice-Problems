399c4962180993c634d7d2c038519b1a1cd28356
ylin10 <ylin10@bu.edu>
2017-01-08 22:45:50 -0800

    Printing out logic before starting problem

--- RotateMatrix.java
+++ RotateMatrix.java
@@ -6,13 +6,13 @@ public class RotateMatrix {
         
         String[][] matrix = { {"A","B","C","D"}, {"E","F","G","H"},{"J","K","L","M"}, {"N","O","P","Q"} };
         
-        int count = 0;
-        
+       //Traversal of matrix to print out all for reference
+        System.err.println("Traversal of matrix");
+                          
         for (int x = 0; x < matrix.length; x++) {
             
             for (int y = 0; y < matrix[x].length; y++) {
-             
-                //System.out.println(matrix[x]);
+                
                 System.out.print(matrix[x][y]);
                 
             }
@@ -21,11 +21,28 @@ public class RotateMatrix {
             
         }
         
-        //System.out.println(matrix[0]);
-        //System.out.println(matrix[1]);
-        //System.out.println(matrix[2]);
-        //System.out.println(matrix[3]);
-        //System.out.println(matrix[4]);
+        
+        System.err.println("**CORNERS**");
+        System.err.println(matrix[0][0]);
+        System.err.println(matrix[0][3]);
+        System.err.println(matrix[3][0]);
+        System.err.println(matrix[3][3]);
+        
+        System.err.println("**INNERS 1**");
+        System.err.println(matrix[0][1]);
+        System.err.println(matrix[1][3]);
+        System.err.println(matrix[3][1]);
+        System.err.println(matrix[1][0]);
+        
+        System.err.println("**INNERS 2**");
+        System.err.println(matrix[0][2]);
+        System.err.println(matrix[2][3]);
+        System.err.println(matrix[3][2]);
+        System.err.println(matrix[2][0]);
+        
+        
+        //System.out.println("matrix length = " + matrix.length);
+        //System.out.println("matrix within array length = " + matrix[0].length);
         
     }
     
