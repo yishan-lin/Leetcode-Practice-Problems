637ce4bd6683721d47483550e9dfbaa7567f5ce8
ylin10 <ylin10@bu.edu>
2017-01-09 10:36:57 -0800

    Final solution
    Print out a staircase with N

--- Staircase.java
+++ Staircase.java
@@ -2,7 +2,25 @@
 
 public class Staircase {
     
+    static void staircaseprint(int height) {
+        
+        for (int x = 0; x <= height; x++) {
+            
+            for (int y = 1; y <= x; y++) {
+                
+                System.out.print("#");
+
+            }
+            
+           System.out.println(""); 
+        }
+    }
+    
     public static void main(String[] args) {
+        
+        int x = 7;
+        
+        staircaseprint(x);
         
     }
     
